00b0335b3ab75d8d050c65f678e4759fb57746cf
liyijian <liyj.1@jifenn.com>
2022-06-10 14:48:54 +0800

    feature:新增异步任务服务
    1.新增asynq服务端，客户端

--- internal/service/v1/apisign/apisign.go
+++ internal/service/v1/apisign/apisign.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	pb "ghub/api/v1/apisign"
 	"github.com/china-xs/gin-tpl/pkg/api_sign"
@@ -10,6 +11,7 @@ import (
 	"github.com/parkingwang/go-sign"
 	"github.com/spf13/viper"
 	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -18,17 +20,24 @@ type ApisignService struct {
 	V *viper.Viper
 }
 
+const (
+	JWT_USER_ID = "jwt_user_id"
+)
+
 func NewApisignService(v *viper.Viper) *ApisignService {
 	return &ApisignService{
 		V: v,
 	}
 }
 
-func (s *ApisignService) ApisignGetDemo(ctx *gin.Context, req *pb.SignRequest) (*pb.SignReply, error) {
+//验证接口签名 demo
+///api/v1/get-sign?name=李先后&id=12&time_stamp=1654756018&nonce_str=s1231sdfsdhfwerwe&appid=1231231abc&sign=05d7e4b9cbfe26b037421cd228089c67
+func (s *ApisignService) ApisignCheckDemo(ctx *gin.Context, req *pb.SignRequest) (*pb.SignReply, error) {
 	return &pb.SignReply{}, nil
 }
 
 //生成api 签名demo
+///api/v1/get-sign?name=李先后&id=12&time_stamp=1654756018&nonce_str=s1231sdfsdhfwerwe&appid=1231231abc
 func (s *ApisignService) CreateSignDemo(ctx *gin.Context, req *pb.CreateSignRequest) (*pb.CreateSignReply, error) {
 	options, err := api_sign.NewOps(s.V)
 	if err != nil {
@@ -59,14 +68,14 @@ func (s *ApisignService) CreateSignDemo(ctx *gin.Context, req *pb.CreateSignRequ
 	}, nil
 }
 
-//生成token demo
+//生成jwt-token demo
 func (s *ApisignService) CreateTokenDemo(ctx *gin.Context, req *pb.CreateTokenRequest) (*pb.CreateTokenReply, error) {
 	options, err := jwt_auth.NewOps(s.V)
 	if err != nil {
 		return nil, err
 	}
 	token, err := createToken(options.Secret, map[string]interface{}{
-		"user_id": "256",
+		JWT_USER_ID: "256",
 	}, 3600)
 
 	if err != nil {
@@ -79,6 +88,7 @@ func (s *ApisignService) CreateTokenDemo(ctx *gin.Context, req *pb.CreateTokenRe
 
 }
 
+//生成jwt-token
 func createToken(secretKey string, payloads map[string]interface{}, seconds int64) (string, error) {
 	now := time.Now().Unix()
 	claims := make(jwt.MapClaims)
@@ -92,4 +102,27 @@ func createToken(secretKey string, payloads map[string]interface{}, seconds int6
 	token.Claims = claims
 
 	return token.SignedString([]byte(secretKey))
+}
+
+//解析jwt-token中的登录信息
+func (s *ApisignService) GetTokenInfo(ctx *gin.Context, req *pb.GetTokenInfoRequest) (*pb.GetTokenInfoReply, error) {
+	userId, err := GetJwtUserIdFromCtx(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetTokenInfoReply{
+		UserId: userId,
+	}, nil
+}
+
+//获取登陆后的user_id demo
+func GetJwtUserIdFromCtx(ctx *gin.Context) (int64, error) {
+	_userId := ctx.GetString(JWT_USER_ID)
+	userId, err := strconv.ParseInt(_userId, 10, 64)
+	if err != nil {
+		return 0, errors.New("未登录")
+	}
+
+	return userId, nil
 }
